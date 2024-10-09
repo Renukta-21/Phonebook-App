8a0acb26373aeda09a6cccce84b1fb534d29c4aa
Deadstroke <edu211004@gmail.com>
2024-10-08 22:55:36 -0600

    first commit

---
+++ Back/server.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+let contactsList = [
+    { id: 1, name: "Alice Johnson", number: "123-456-7890" },
+    { id: 2, name: "Bob Smith", number: "234-567-8901" },
+    { id: 3, name: "Charlie Brown", number: "345-678-9012" },
+    { id: 4, name: "David Wilson", number: "456-789-0123" },
+    { id: 5, name: "Eva Green", number: "567-890-1234" },
+    { id: 6, name: "Frank White", number: "678-901-2345" },
+    { id: 7, name: "Grace Lee", number: "789-012-3456" },
+    { id: 8, name: "Hank Miller", number: "890-123-4567" },
+    { id: 9, name: "Ivy Davis", number: "901-234-5678" },
+    { id: 10, name: "Jack Thompson", number: "012-345-6789" }
+  ];
+  
+const welcomeText = 
+`<h1>Phonebook backend</h1>
+<p>Access <b>/contacts    </b> for full list</p>
+<p>Access <b>/contacts/:id</b> for specific contact</p>`
+
+app.get('/', (req, res)=>{
+    res.send(welcomeText)
+})
+
+app.get('/contacts', (req, res)=>{
+    res.send(contactsList)
+})
+app.get('/contacts/:id', (req, res) => {
+    const { id } = req.params; // Obtén el id de los parámetros de la solicitud
+    const foundElm = contactsList.find(c=> c.id==id)
+    if(foundElm){
+        res.send(foundElm);
+    }else{
+        res.status(404).send({error: 'Contact not found'})
+    }
+});
+
+app.delete(`/contacts/:id`, (req, res)=>{
+    const {id} = req.params
+    const deleteID = Number(id)
+    const contactExists = contactsList.some(c=> c.id === deleteID)
+
+    if(contactExists){
+        contactsList = contactsList.filter(c=> c.id !== Number(id))
+        console.log('Contact deleted succesfully')
+        res.status(204).end()
+    }else{
+        res.status(404).send({error: 'contact not found'})
+    }
+})
+app.post('/contacts', (req, res)=>{
+    const newElement = req.body
+    contactsList = [...contactsList, newElement]
+    res.status(201).end()
+})
+app.put('/contacts/:id', (req,res)=>{
+    const {id} = req.params
+    const body = req.body
+    contactsList = contactsList.map(c=> c.id!==Number(id) ? c : body) 
+    console.log(body)
+    res.status(200).send(body)
+})
+app.listen(5000,()=>
+console.log('server started on 3000'))
---
+++ Front/src/App.jsx
@@ -0,0 +1,111 @@
+import { useEffect, useState } from "react"
+
+function App() {
+  const [contacts, setContacts] = useState([])
+  const [name, setName] = useState("")
+  const [number, setNumber] = useState("")
+  const [editingIndex, setEditingIndex] = useState(null)
+  const [editingReg, setEditingReg] = useState({ name: '', number: '' })
+
+  useEffect(() => {
+    fetch('http://localhost:5000/contacts')
+      .then(res => res.json())
+      .then(data => setContacts(data))
+    setEditingIndex(null)
+  }, [])
+
+  function handleDelete(id) {
+    fetch(`http://localhost:5000/contacts/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(() => setContacts(prevContacts=> prevContacts.filter(c=> c.id!== id)))
+      .catch(err => console.error(err))
+  }
+  function handleUpdate(id) {
+    setEditingIndex(id)
+    const updateElement = contacts.find(c => c.id === id)
+    setEditingReg({ name: updateElement.name, number: updateElement.number })
+
+    if (editingIndex === id) {
+      setEditingIndex(null)
+      fetch(`http://localhost:5000/contacts/${id}`, {
+        method: "PUT",
+        headers: {
+          'Content-Type': 'application/json'
+        }, body: JSON.stringify({id, ...editingReg})
+      }).then(res => res.json())
+      .then(data=> setContacts(prevContacts=>{
+        return(
+          prevContacts.map(c=>c.id!==id? c: data)
+        )
+      }))
+    }
+
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const maxId = contacts.reduce((max, contact) => {
+      return contact.id > max ? contact.id : max
+    }, 0)
+    const objForm = {
+      id: Number(maxId) + 1, name, number
+    }
+    fetch(`http://localhost:5000/contacts`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(objForm)
+    }).then(res=> setContacts(prevContacts => [...prevContacts, objForm]))
+    setName("")
+    setNumber("")
+  }
+  return (
+    <div>
+      <h3>PhoneBook App</h3>
+      <form action="" onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label><input type="text" id="name" value={name} onChange={e => setName(e.target.value)} />
+        <br />
+        <label htmlFor="number">Number:</label><input type="text" id="number" value={number} onChange={e => setNumber(e.target.value)} />
+        <br />
+        <button type="submit">Add</button>
+      </form>
+      <h3>Numbers</h3>
+      <div style={{display:'flex', flexWrap:'wrap', gap:'10px'}}>
+      {contacts.map((cont) => {
+        return (
+          <div key={cont.id} style={{height:'200px', aspectRatio:'1', border:'1px solid black', padding:'15px'}}>
+            <small style={{backgroundColor:'black', color:'white', padding:'10px', borderRadius:'50%'}}>{cont.id}</small>
+            {editingIndex === cont.id ?
+              <div>
+                <label htmlFor="nameUpd">Name:</label>
+                <input type="text" id="nameUpd" value={editingReg.name}
+                  onChange={e => {
+                    setEditingReg(prevData => ({ ...prevData, name: e.target.value }))
+                  }} />
+                <br />
+                <label htmlFor="numbUpd">Number:</label>
+                <input type="text" id="numbUpd" value={editingReg.number}
+                  onChange={e => {
+                    setEditingReg(prevData => ({ ...prevData, number: e.target.value }))
+                  }} />
+              </div>
+              : <div>
+                <p>Name:    {cont.name}</p>
+                <p>Number:  {cont.number}</p></div>}
+            <br />
+            <button onClick={() => handleDelete(cont.id)}>Delete</button>
+            <button style={{margin:'0px 10px'}} onClick={() => handleUpdate(cont.id)}>{editingIndex === cont.id ? 'Done' : 'Edit'}</button>
+            <hr />
+          </div>
+        )
+      })}
+      </div>
+    </div>
+  )
+}
+
+export default App
